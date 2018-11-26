223301380561c5ebb9e513ddc29128f86a1270a0
Bruno Toledo <btoledo1973@gmail.com>
2018-11-26 19:23:24 -0200

    validates included

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,9 @@
 class Route < ApplicationRecord
   belongs_to :user
+
+  validates :start_date_time, presence: true
+  validates :end_date_time, presence: true
+  validates :origin, presence: true
+  validates :destination, presence: true
+  validates :capacity, presence: true
 end
