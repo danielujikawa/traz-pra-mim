cf24480f16697b86d575b45cfcdc68c20b800f62
Bruno Toledo <btoledo1973@gmail.com>
2018-11-27 14:04:38 -0200

    routes ok. create sendo feito

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -7,9 +7,18 @@ class RoutesController < ApplicationController
   end
 
   def new
+    @user = current_user
+    @route = Route.new
   end
 
   def create
+    @route = Route.new(route_params)
+    @route.user = current_user
+    if @route.save
+      redirect_to routes_path(@route)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,7 +32,7 @@ class RoutesController < ApplicationController
 
   private
 
-  def cocktail_params
+  def route_params
     params.require(:route).permit(:start_date_time, :end_date_time, :origin, :destination, :capacity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,35 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :users, only: [] do
+    resources :routes
+  end
+
+  # resoureces :routes do
+  #   resources :orders
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'users/new', to: 'users#new', as: :new_user
   # post 'users', to: 'users#create'
   # get 'users/:id', to: 'users#show', as: :user
   # get 'users/:id/edit', to: 'users#edit', as: :edit_user
   # patch 'users/:id', to: 'users#update'
-  # delete 'users/:id', to: 'users#destroy'
-  get 'routes/index', to: 'routes#index'
-  get 'routes/new', to: 'routes#new', as: :new_route
-  post 'routes', to: 'routes#create'
-  get 'routes/:id', to: 'routes#show', as: :route
-  get 'routes/:id/edit', to: 'routes#edit', as: :edit_route
-  patch 'routes/:id', to: 'routes#update'
-  delete 'routes/:id', to: 'routes#destroy'
+  # # delete 'users/:id', to: 'users#destroy'
+  # get 'routes/index', to: 'routes#index'
+  # get 'routes/new', to: 'routes#new', as: :new_route
+  # post 'routes', to: 'routes#create'
+  # get 'routes/:id', to: 'routes#show', as: :route
+  # get 'routes/:id/edit', to: 'routes#edit', as: :edit_route
+  # patch 'routes/:id', to: 'routes#update'
+  # delete 'routes/:id', to: 'routes#destroy'
 
-  get 'orders/index', to: 'orders#index'
-  get 'orders/new', to: 'orders#new', as: :new_order
-  post 'orders', to: 'orders#create'
-  get 'orders/:id', to: 'orders#show', as: :order
-  get 'orders/:id/edit', to: 'orders#edit', as: :edit_order
-  patch 'orders/:id', to: 'orders#update'
-  delete 'orders/:id', to: 'orders#destroy'
+  # get 'orders/index', to: 'orders#index'
+  # get 'orders/new', to: 'orders#new', as: :new_order
+  # post 'orders', to: 'orders#create'
+  # get 'orders/:id', to: 'orders#show', as: :order
+  # get 'orders/:id/edit', to: 'orders#edit', as: :edit_order
+  # patch 'orders/:id', to: 'orders#update'
+  # delete 'orders/:id', to: 'orders#destroy'
 
 end
 
