bcc0af9cbb4498f36e41f37ad09ea4e73e99b1c2
laura mft <lauramonfort@live.fr>
2018-11-27 13:50:03 -0200

    Db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Routes.destroy_all
+
+puts 'Creating routes...'
+routes_attributes = [
+  {
+    start_date_time: '01/12/2018',
+    end_date_time: '02/12/2018',
+    origin: 'Sao Paulo',
+    destination: 'Rio de janeiro',
+    capacity: 'small',
+  },
+  {
+    start_date_time: '01/12/2018',
+    end_date_time: '02/12/2018',
+    origin: 'Sao Paulo',
+    destination: 'Rio de janeiro',
+    capacity: 'small',
+  },
+  {
+    start_date_time: '01/12/2018',
+    end_date_time: '02/12/2018',
+    origin: 'Sao Paulo',
+    destination: 'Rio de janeiro',
+    capacity: 'small',
+  },
+  {
+    start_date_time: '01/12/2018',
+    end_date_time: '02/12/2018',
+    origin: 'Sao Paulo',
+    destination: 'Rio de janeiro',
+    capacity: 'small',
+  },
+  {
+    start_date_time: '01/12/2018',
+    end_date_time: '02/12/2018',
+    origin: 'Sao Paulo',
+    destination: 'Rio de janeiro',
+    capacity: 'small',
+  }
+
+]
+
+Route.create!(Routes_attributes)
+puts 'Finished!'
+
+
+puts 'Cleaning database...'
+Users.destroy_all
+
+puts 'Creating Users...'
+Users_attributes = [
+  {
+    email: example@gmail.com ,
+    encrypted_password: '123',
+
+  },
+]
+User.create!(users_attributes)
+puts 'Finished!'
