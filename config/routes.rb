fcee51bd6b15110e3b48e6c023ae166754af3718
laura mft <lauramonfort@live.fr>
2018-11-27 17:46:05 -0200

    addfooterlinktosymbol

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :users, only: [] do
-    resources :routes
+    resources :routes, only: [:index]
   end
 
   # resoureces :routes do
