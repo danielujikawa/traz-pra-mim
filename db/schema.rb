98fef7605bc7d99ad98e6cbe997228bf31414e1a
laura mft <lauramonfort@live.fr>
2018-11-29 17:39:41 -0200

    Add review to show page  not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_170109) do
+ActiveRecord::Schema.define(version: 2018_11_29_182830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2018_11_29_170109) do
     t.datetime "updated_at", null: false
     t.index ["route_id"], name: "index_orders_on_route_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -63,5 +72,6 @@ ActiveRecord::Schema.define(version: 2018_11_29_170109) do
 
   add_foreign_key "orders", "routes"
   add_foreign_key "orders", "users"
+  add_foreign_key "reviews", "users"
   add_foreign_key "routes", "users"
 end
