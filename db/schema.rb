b4da90c22d4f5ecfb7df38cea81484bc786b4300
Daniel Ujikawa <danielujikawa@gmail.com>
2018-11-29 15:00:51 -0200

    clean-master

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2018_11_29_160457) do
     t.index ["user_id"], name: "index_routes_on_user_id"
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.string "type"
+    t.string "pick_up_address"
+    t.string "delivery_address"
+    t.string "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
